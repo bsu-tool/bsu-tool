f654124a15c65bc128940d14f44842d6b300ede0
AliciaCurths <147432816+AliciaCurths@users.noreply.github.com>
2026-05-11 17:42:01 -0700

    Update docstring's import path for PcapNgReader and EnhancedPacketBlock

--- bsu_tool/pcapng/__init__.py
+++ bsu_tool/pcapng/__init__.py
@@ -6,7 +6,7 @@ separate module so the two layers can be tested independently.
 
 Typical use::
 
-    from bsu_tool.parser import PcapNgReader, EnhancedPacketBlock
+    from pcapng.parser import PcapNgReader, EnhancedPacketBlock
 
     with open("capture.pcapng", "rb") as fp:
         for block in PcapNgReader(fp):
